d4833a97d643e4cbb13655664be71c53a391d11f
esraash10 <esraashafaa660@gmail.come>
2023-02-15 12:04:06 +0200

    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

--- routes/index.js
+++ routes/index.js
@@ -16,16 +16,28 @@ router.get('/', function(req, res, next) {
   const queryData = url.parse(req.url, true).query;
   connection.query("SELECT grade FROM  user WHERE id="+queryData['id']+"" ,function(error,results) {
       console.log(results);
-      res.json(results);
-  var json_data = JSON.parse(JSON.stringify(results));
-      connection.query('SELECT * FROM subject WHERE grade="'+ json_data['grade']+'"', function (error, result) {
+    var json_data = JSON.parse(JSON.stringify(results));``
+      connection.query('SELECT * FROM subject WHERE grade="'+ json_data[0]['grade']+'"', function (error, result) {
           console.log(result);
           res.json(result);
       });
   })
   });
+router.get('/index', function(req, res, next) {
+    var queryData = url.parse(req.url, true).query;
+    connection.query("SELECT grade FROM  user WHERE id="+queryData['id']+"" ,function(error,results) {
+        console.log(results);
+   var json_data = JSON.parse(JSON.stringify(results));
+        connection.query('SELECT * FROM subject WHERE grade="'+json_data[0]['grade']+'"', function (error, result) {
+            console.log(result);
+           var json_data = JSON.parse(JSON.stringify(result));
+          connection.query('SELECT * FROM course WHERE subject="'+json_data[0]['subject']+'"', function (error, resulte) {
+       console.log(resulte);
+       res.json(resulte);
+    });
+    });
 
-
-
+    })
+});
 
 module.exports = router;
